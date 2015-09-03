841b209bb1c8b46d7a74f70e602ca41446c1cbb2
jbowkett <james@bowkett.info>
2015-09-03 17:43:12 +0000

    Code tidyup for readibility

--- jmeter/src/main/java/com/excelian/mache/jmeter/mongo/MultiThreadReadFromMongo.java
+++ jmeter/src/main/java/com/excelian/mache/jmeter/mongo/MultiThreadReadFromMongo.java
@@ -1,14 +1,13 @@
 package com.excelian.mache.jmeter.mongo;
 
-import org.apache.jmeter.config.Arguments;
-import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
-import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.util.JMeterUtils;
-
 import com.excelian.mache.core.SchemaOptions;
 import com.excelian.mache.jmeter.MacheAbstractJavaSamplerClient;
 import com.excelian.mache.mongo.MongoDBCacheLoader;
 import com.mongodb.ServerAddress;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.util.JMeterUtils;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -16,9 +15,9 @@ import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MultiThreadReadFromMongo extends MacheAbstractJavaSamplerClient {
-	private static final long serialVersionUID = 1193347578889214521L;
-	
-	private MongoDBCacheLoader<String, MongoTestEntity> db;
+    private static final long serialVersionUID = 1193347578889214521L;
+
+    private MongoDBCacheLoader<String, MongoTestEntity> db;
 
     @Override
     public void setupTest(JavaSamplerContext context) {
@@ -28,9 +27,9 @@ public class MultiThreadReadFromMongo extends MacheAbstractJavaSamplerClient {
 
         try {
             db = new MongoDBCacheLoader<>(MongoTestEntity.class, new CopyOnWriteArrayList<>(
-                    Arrays.asList(new ServerAddress(mapParams
-                            .get("mongo.server.ip.address"), 27017))),
-            SchemaOptions.CREATEANDDROPSCHEMA, mapParams.get("keyspace.name"));
+                Arrays.asList(new ServerAddress(mapParams
+                    .get("mongo.server.ip.address"), 27017))),
+                SchemaOptions.CREATEANDDROPSCHEMA, mapParams.get("keyspace.name"));
             db.create();//ensure we are connected and schema exists
         } catch (Exception e) {
             getLogger().error("Error connecting to cassandra", e);
@@ -41,7 +40,7 @@ public class MultiThreadReadFromMongo extends MacheAbstractJavaSamplerClient {
     public void teardownTest(JavaSamplerContext context) {
         if (db != null) db.close();
     }
-    
+
     @Override
     public SampleResult runTest(JavaSamplerContext context) {
         Map<String, String> mapParams = extractParameters(context);
@@ -57,22 +56,22 @@ public class MultiThreadReadFromMongo extends MacheAbstractJavaSamplerClient {
 
         do {
             try {
-				readEntity = db.load(e.pkString);
+                readEntity = db.load(e.pkString);
 
-	            if (readEntity == null) {
-	                throw new Exception("No data found in db for key value of " + e.pkString);
-	            }
+                if (readEntity == null) {
+                    throw new Exception("No data found in db for key value of " + e.pkString);
+                }
 
-	            try {
-	                Thread.sleep(sleepMilis);
-	            } catch (InterruptedException e1) {
-	                getLogger().error("Error while reading value through cache from mongo " + e1.getMessage(), e1);
-	                return super.setupResultForError(result, e1);
-	            }
-			} catch (Exception ex) {
-	            setupResultForError(result, ex);
-	            return result;
-			}
+                try {
+                    Thread.sleep(sleepMilis);
+                } catch (InterruptedException e1) {
+                    getLogger().error("Error while reading value through cache from mongo " + e1.getMessage(), e1);
+                    return super.setupResultForError(result, e1);
+                }
+            } catch (Exception ex) {
+                setupResultForError(result, ex);
+                return result;
+            }
         }
         while (new Date().getTime() - startTime <= timeoutMs && (readEntity == null || !e.description.equals(readEntity.description)));
 
@@ -83,11 +82,11 @@ public class MultiThreadReadFromMongo extends MacheAbstractJavaSamplerClient {
         return result;
     }
 
-	protected MongoTestEntity initMongoEntity(Map<String, String> mapParams) {
-		return new MongoTestEntity(mapParams.get("entity.key"),
-				JMeterUtils.getProperty(mapParams.get("entity.value.name")));
-	}
-	
+    protected MongoTestEntity initMongoEntity(Map<String, String> mapParams) {
+        return new MongoTestEntity(mapParams.get("entity.key"),
+            JMeterUtils.getProperty(mapParams.get("entity.value.name")));
+    }
+
     @Override
     public Arguments getDefaultParameters() {
         final Arguments result = super.getDefaultParameters();
--- jmeter/src/main/java/com/excelian/mache/jmeter/mongo/MultiThreadWriteToMongo.java
+++ jmeter/src/main/java/com/excelian/mache/jmeter/mongo/MultiThreadWriteToMongo.java
@@ -1,25 +1,24 @@
 package com.excelian.mache.jmeter.mongo;
 
-import org.apache.jmeter.config.Arguments;
-import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
-import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.util.JMeterUtils;
-
 import com.excelian.mache.core.SchemaOptions;
 import com.excelian.mache.jmeter.MacheAbstractJavaSamplerClient;
 import com.excelian.mache.mongo.MongoDBCacheLoader;
 import com.fasterxml.uuid.Generators;
 import com.mongodb.ServerAddress;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.util.JMeterUtils;
 
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MultiThreadWriteToMongo extends
-MacheAbstractJavaSamplerClient {
-	private static final long serialVersionUID = 2529296764873734718L;
-	
-	private MongoDBCacheLoader<String, MongoTestEntity> db;
+    MacheAbstractJavaSamplerClient {
+    private static final long serialVersionUID = 2529296764873734718L;
+
+    private MongoDBCacheLoader<String, MongoTestEntity> db;
 
     @Override
     public void setupTest(JavaSamplerContext context) {
@@ -28,9 +27,9 @@ MacheAbstractJavaSamplerClient {
 
         try {
             db = new MongoDBCacheLoader<>(MongoTestEntity.class, new CopyOnWriteArrayList<>(
-                    Arrays.asList(new ServerAddress(mapParams
-                            .get("mongo.server.ip.address"), 27017))),
-            SchemaOptions.CREATEANDDROPSCHEMA, mapParams.get("keyspace.name"));
+                Arrays.asList(new ServerAddress(mapParams
+                    .get("mongo.server.ip.address"), 27017))),
+                SchemaOptions.CREATEANDDROPSCHEMA, mapParams.get("keyspace.name"));
             db.create();
         } catch (Exception e) {
             getLogger().error("Error connecting to mongo", e);
@@ -41,44 +40,44 @@ MacheAbstractJavaSamplerClient {
     public void teardownTest(JavaSamplerContext context) {
         if (db != null) db.close();
     }
-    
-	@Override
-	public SampleResult runTest(JavaSamplerContext context) {
-		Map<String, String> mapParams = extractParameters(context);
-		final SampleResult result = new SampleResult();
 
-		result.sampleStart();
+    @Override
+    public SampleResult runTest(JavaSamplerContext context) {
+        Map<String, String> mapParams = extractParameters(context);
+        final SampleResult result = new SampleResult();
 
-		try {
+        result.sampleStart();
 
-			MongoTestEntity e = initMongoEntity(mapParams);
-			db.put(e.pkString, e);
-			JMeterUtils.setProperty(mapParams.get("entity.value.name"), e.description);
+        try {
 
-			result.sampleEnd();
-			result.setSuccessful(true);
-			result.setResponseMessage("Put value (" + e.description
-					+ ") from Cache");
-		} catch (Exception e1) {
-			return super.setupResultForError(result, e1);
-		}
+            MongoTestEntity e = initMongoEntity(mapParams);
+            db.put(e.pkString, e);
+            JMeterUtils.setProperty(mapParams.get("entity.value.name"), e.description);
 
-		return result;
-	}
+            result.sampleEnd();
+            result.setSuccessful(true);
+            result.setResponseMessage("Put value (" + e.description
+                + ") from Cache");
+        } catch (Exception e1) {
+            return super.setupResultForError(result, e1);
+        }
 
-	protected MongoTestEntity initMongoEntity(Map<String, String> mapParams) {
-		return new MongoTestEntity(mapParams.get("entity.key"),
-				Generators.timeBasedGenerator().generate().toString());
-	}
+        return result;
+    }
 
-	@Override
-	public Arguments getDefaultParameters() {
-		Arguments defaultParameters = super.getDefaultParameters();
+    protected MongoTestEntity initMongoEntity(Map<String, String> mapParams) {
+        return new MongoTestEntity(mapParams.get("entity.key"),
+            Generators.timeBasedGenerator().generate().toString());
+    }
+
+    @Override
+    public Arguments getDefaultParameters() {
+        Arguments defaultParameters = super.getDefaultParameters();
         defaultParameters.addArgument("mongo.server.ip.address", "10.28.1.140");
         defaultParameters.addArgument("activemq.connection", "vm://localhost");
         defaultParameters.addArgument("entity.key", "K${loopCounter}");
-		defaultParameters.addArgument("entity.value.name", "mongo.entity.value");
+        defaultParameters.addArgument("entity.value.name", "mongo.entity.value");
         defaultParameters.addArgument("keyspace.name", "JMeterNoCache");
-		return defaultParameters;
-	}
+        return defaultParameters;
+    }
 }
